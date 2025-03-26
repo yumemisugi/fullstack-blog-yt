6d21058e7ba2c7f1ee12461f55c997f6ccd5e5bf
yumemisugi <theoriacode.yt@gmail.com>
2025-03-26 16:42:07 +0900

    ✨ POSTできるようになったところまで！（24:00）

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -11,6 +11,22 @@ export async function main() {
     }
 }
 
+//ブログ投稿用API
+export const POST = async (req: Request, res: NextResponse) => {
+    try{
+        const { title, description } = await req.json();
+        
+        await main();
+        const posts = await prisma.post.create({ data: { title, description }});
+        return NextResponse.json({ message: "Success", posts }, { status: 201 });
+    } catch(err) {
+        return NextResponse.json({ message: "Error", err },{ status: 500 })
+    } finally {
+        await prisma.$disconnect();
+    } 
+};
+
+
 //ブログの全記事取得API
 export const GET = async (req: Request, res: NextResponse) => {
     try{
